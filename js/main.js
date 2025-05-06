b21442b9c3bdc69a520c1c5de3c72741c8a7a2e4
mdcelenexn <vanessapugaperez@gmail.comm>
2025-05-06 21:36:55 +0200

    nuevo

---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+const InputAside=document.querySelector(".js-aside");
+const btnAside=document.querySelector(".js-btn-tareas");
+const inputBuscar=document.querySelector(".js-inputBuscar");
+const btnBuscar=document.querySelector(".js-inputBuscar");
+const Ul =document.querySelector(".js-ul");
+
+console.log("funcionaaaaa")
+
+
+
+
+function handleClick(event) {
+
+    console.log("fubnciona")
+
+
+}
+
+btnAside.addEventListener("click", handleClick);
+

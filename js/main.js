9e0dd53523d60d2478e163c0fac8c5066d14afec
mdcelenexn <vanessapugaperez@gmail.comm>
2025-05-07 17:34:25 +0200

    nuevos estilos CSS

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
+'use strict';
 
 const InputAside=document.querySelector(".js-aside");
 const btnAside=document.querySelector(".js-btn-tareas");
-const inputBuscar=document.querySelector(".js-inputBuscar");
-const btnBuscar=document.querySelector(".js-inputBuscar");
-const Ul =document.querySelector(".js-ul");
+const inputForm=document.querySelector(".js-inputBuscar");
+const btnForm=document.querySelector(".js-btn-Buscar");
+const listElement = document.querySelector('.js-ul');
 
 console.log("funcionaaaaa")
 
@@ -12,10 +13,20 @@ console.log("funcionaaaaa")
 
 function handleClick(event) {
 
-    console.log("fubnciona")
+    console.log("funciona boton aside")
 
 
 }
+
+
+
+
+
+
+
+
+
+
 
 btnAside.addEventListener("click", handleClick);
 
